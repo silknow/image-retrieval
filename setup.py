7bfc2436e6185eb3cbb9b6e780ddced435e7a9d8
Thibault Ehrhart <thibault.ehrhart@gmail.com>
2021-06-09 16:39:29 +0200

    Add xlswriter, scikit-image, to setup.py reqs

--- setup.py
+++ setup.py
@@ -28,7 +28,9 @@ all_reqs = ['#Dev/Deployment',
 			'tensorflow-hub==0.6.0',
 			'matplotlib',
 			'sklearn',
-            'scipy']
+            'scipy',
+            'xlsxwriter',
+            'scikit-image']
 
 install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
 dependency_links = [x.strip().replace('git+', '') for x in all_reqs if x.startswith('git+')]
